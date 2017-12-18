f0ea582ee3c886d4f8df5110e4bc2a21cc523c48
Harvey Wang <Harvey@Harveys-MacBook-Pro.local>
2017-12-18 00:07:30 -0400

    Added first C translation

---
+++ C/Problem1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3, b = 5, c = 15, sum = 0;
+    while (a < 1000) {
+        sum += a;
+        a += 3;
+    }
+    while (b < 1000) {
+        sum += b;
+        b += 5;
+    }
+    while (c < 1000) {
+        sum -= c;
+        c += 15;
+    }
+    printf("%d\n", sum);
+}
