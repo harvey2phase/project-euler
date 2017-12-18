6ee65806a75f4981a473e8924b43abe99a79780f
Harvey Wang <Harvey@Harveys-MacBook-Pro.local>
2017-12-18 01:01:50 -0400

    #29 solved

---
+++ 29_Distinct_powers.py
@@ -0,0 +1,18 @@
+# Created December 2017
+
+l = []
+b = 2
+for i in range(2, 101):
+    a = i
+    for b in range(2, 101):
+        a *= i
+        l.append(a)
+
+l.sort()
+i = 0
+while i < len(l)-1:
+    if l[i] == l[i+1]:
+        del(l[i+1])
+        i -= 1
+    i += 1
+print(len(l))
